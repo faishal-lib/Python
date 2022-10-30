efc6a806f6c21b0116911e66b837aa72a3d76226
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 21:06:05 +0700

    combine if + function

---
+++ Intermediate Python/Combine IF + Function/function_list.py
@@ -0,0 +1,7 @@
+def packet1():
+    print ("You're Selected packet 1")
+    print ("Insert Function Here" +"\n")
+
+def packet2():
+    print ("You're Selected packet 2")
+    print ("Insert Function Here" +"\n")
---
+++ Intermediate Python/Combine IF + Function/select_function.py
@@ -0,0 +1,16 @@
+from function_list import *
+
+print ("\n")
+print ("===== SELECT PACKET =====")
+print ("1. Packet 1" + "\n" + "2. Packet 2")
+print ("\n")
+select = int(input("Choose Number : "))
+
+if select == 1 :
+    packet1()
+
+elif select == 2 :
+    packet2()
+
+else :
+    print ("Error Input ..") 
