39cfc2e84f2f4118b4602f574bc8d47d0977ab20
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 15:58:53 +0700

    error identation

---
+++ Basic Python/Python If...Else/identation.py
@@ -0,0 +1,11 @@
+# Error : IndentationError: expected an indented block
+# Dont forget to add whitespace or tab at the beginning of a line
+
+a = 12
+b = 7
+
+if a > b :
+print ("a is greater than b")
+
+else :
+print ("a is smaller than b")
