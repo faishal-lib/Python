1ff3ed2308cf90837d5371d86df457407a4d7653
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 16:44:13 +0700

    Use operator (or) to combine 2 condition, at least 1 condition is TRUE

---
+++ Basic Python/Python If...Else/or_condition.py
@@ -0,0 +1,11 @@
+# Use operator (or) to combine 2 condition, at least 1 condition is TRUE
+
+a = 350
+b = 250
+c = 300
+
+if a > c or b > c : 
+    print ("one of the conditions is TRUE")
+
+else :
+    print ("Error")
