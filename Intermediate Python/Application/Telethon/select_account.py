373d955c6497a5b3df58fbc06b7c946ac0933731
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 17:35:56 +0700

    Version 1

---
+++ Intermediate Python/Application/Telethon/select_account.py
@@ -0,0 +1,16 @@
+def packet1():
+    print ("Anda memilih Packet 1")
+
+def packet2():
+    print ("Anda memilih Packet 2")
+
+select = int(input("Choose Number : "))
+
+if select == 1 :
+    packet1()
+
+elif select == 2 :
+    packet2()
+
+else :
+    print ("Error")
