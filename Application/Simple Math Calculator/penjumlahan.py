89cee0bf6366c95e8a9cd46df955227d7f24d911
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 13:18:18 +0700

    Version 1.0

--- Application/Simple Math Calculator/penjumlahan.py
+++ Application/Simple Math Calculator/penjumlahan.py
@@ -8,6 +8,6 @@ bil2 = input("Input Bilangan 2 : ")
 # Misal : 6+6 = 66 bukan 12
 hasil = (int(bil1)+ int(bil2))
 
-# String (str) perlu didkelarasikan supaya tidak error ketika melakukan print
+# String (str) perlu didkelarasikan supaya tidak error ketika melakukan print 2 tipe data yg berbeda (int dan str)
 # Error yg terjadi : TypeError: 'str' object is not callable
 print ("Hasil Penjumlahan : " +str(hasil))
