021d76144fa35855f0516b0a8fb55836c77a4058
faishal-lib <mochamadfaishal1311@gmail.com>
2022-10-30 16:36:36 +0700

    Use operator (and) to combine 2 condition or more

---
+++ Basic Python/Python If...Else/and_condition.py
@@ -0,0 +1,14 @@
+# Use operator (and) to combine 2 condition or more
+
+a = 100
+b = 250
+c = 300
+
+if a > c and b > c : 
+    print ("A is bigger than B,  B is bigger than C")
+
+elif a < c and b < c : 
+    print ("A is smaller than B,  B is smaller than C")
+
+else :
+    print ("Error")
